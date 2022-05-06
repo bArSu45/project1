2441df94579a71764976b881dec5431c4e622abe
Abhishek Lohia <101470770+abhisheklohia2002@users.noreply.github.com>
2022-05-06 16:16:29 +0530

    Add files via upload

---
+++ body.js
@@ -0,0 +1,303 @@
+var data = [{
+  image_url : "https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dwf97a6473/crop/026414639_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+  brand : "Fragrance",
+  name : "Butterfly",
+  com: "Fine Fragrance Mist",
+  price: "$75",
+  view : "Mix & Match: Buy 3, Get 3 Free or Buy 2, Get 1 Free",
+
+},
+{
+  image_url : "https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dwf97a6473/crop/026414639_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+  brand : "Fragrance",
+  name : "Butterfly",
+  com: "Fine Fragrance Mist",
+  price: "$75",
+  view : "Mix & Match: Buy 3, Get 3 Free or Buy 2, Get 1 Free",
+},
+{
+  image_url : "https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dwf97a6473/crop/026414639_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+  brand : "Fragrance",
+  name : "Butterfly",
+  com: "Fine Fragrance Mist",
+  price: "$75",
+  view : "Mix & Match: Buy 3, Get 3 Free or Buy 2, Get 1 Free",
+},
+{
+  image_url : "https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dwf97a6473/crop/026414639_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+  brand : "Fragrance",
+  name : "Butterfly",
+  com: "Fine Fragrance Mist",
+  price: "$75",
+  view : "Mix & Match: Buy 3, Get 3 Free or Buy 2, Get 1 Free",
+},
+{
+  image_url : "https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dwf97a6473/crop/026414639_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+  brand : "Fragrance",
+  name : "Butterfly",
+  com: "Fine Fragrance Mist",
+  price: "$75",
+  view : "Mix & Match: Buy 3, Get 3 Free or Buy 2, Get 1 Free",
+},
+{
+  image_url : "https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dwf97a6473/crop/026414639_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+  brand : "Fragrance",
+  name : "Butterfly",
+  com: "Fine Fragrance Mist",
+  price: "$75",
+  view : "Mix & Match: Buy 3, Get 3 Free or Buy 2, Get 1 Free",
+},
+{
+  image_url : "https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dwf97a6473/crop/026414639_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+  brand : "Fragrance",
+  name : "Butterfly",
+  com: "Fine Fragrance Mist",
+  price: "$75",
+  view : "Mix & Match: Buy 3, Get 3 Free or Buy 2, Get 1 Free",
+},
+{
+  image_url : "https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dwf97a6473/crop/026414639_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+  brand : "Fragrance",
+  name : "Butterfly",
+  com: "Fine Fragrance Mist",
+  price: "$75",
+  view : "Mix & Match: Buy 3, Get 3 Free or Buy 2, Get 1 Free",
+},
+{
+  image_url : "https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dwf97a6473/crop/026414639_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+  brand : "Fragrance",
+  name : "Butterfly",
+  com: "Fine Fragrance Mist",
+  price: "$75",
+  view : "Mix & Match: Buy 3, Get 3 Free or Buy 2, Get 1 Free",
+},
+{
+  image_url : "https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dwf97a6473/crop/026414639_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+  brand : "Fragrance",
+  name : "Butterfly",
+  com: "Fine Fragrance Mist",
+  price: "$75",
+  view : "Mix & Match: Buy 3, Get 3 Free or Buy 2, Get 1 Free",
+},
+{
+  image_url : "https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dwf97a6473/crop/026414639_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+  brand : "Fragrance",
+  name : "Butterfly",
+  com: "Fine Fragrance Mist",
+  price: "$75",
+  view : "Mix & Match: Buy 3, Get 3 Free or Buy 2, Get 1 Free",
+},
+{
+  image_url : "https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dwf97a6473/crop/026414639_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+  brand : "Fragrance",
+  name : "Butterfly",
+  com: "Fine Fragrance Mist",
+  price: "$75",
+  view : "Mix & Match: Buy 3, Get 3 Free or Buy 2, Get 1 Free",
+},
+{
+  image_url : "https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dwf97a6473/crop/026414639_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+  brand : "Fragrance",
+  name : "Butterfly",
+  com: "Fine Fragrance Mist",
+  price: "$75",
+  view : "Mix & Match: Buy 3, Get 3 Free or Buy 2, Get 1 Free",
+},
+{
+  image_url : "https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dwf97a6473/crop/026414639_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+  brand : "Fragrance",
+  name : "Butterfly",
+  com: "Fine Fragrance Mist",
+  price: "$75",
+  view : "Mix & Match: Buy 3, Get 3 Free or Buy 2, Get 1 Free",
+},
+{
+  image_url : "https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dwf97a6473/crop/026414639_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+  brand : "Fragrance",
+  name : "Butterfly",
+  com: "Fine Fragrance Mist",
+  price: "$75",
+  view : "Mix & Match: Buy 3, Get 3 Free or Buy 2, Get 1 Free",
+},
+{
+  image_url : "https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dwf97a6473/crop/026414639_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+  brand : "Fragrance",
+  name : "Butterfly",
+  com: "Fine Fragrance Mist",
+  price: "$75",
+  view : "Mix & Match: Buy 3, Get 3 Free or Buy 2, Get 1 Free",
+},
+{
+  image_url : "https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dwf97a6473/crop/026414639_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+  brand : "Fragrance",
+  name : "Butterfly",
+  com: "Fine Fragrance Mist",
+  price: "$75",
+  view : "Mix & Match: Buy 3, Get 3 Free or Buy 2, Get 1 Free",
+},
+{
+  image_url : "https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dwf97a6473/crop/026414639_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+  brand : "Fragrance",
+  name : "Butterfly",
+  com: "Fine Fragrance Mist",
+  price: "$75",
+  view : "Mix & Match: Buy 3, Get 3 Free or Buy 2, Get 1 Free",
+},
+{
+  image_url : "https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dwf97a6473/crop/026414639_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+  brand : "Fragrance",
+  name : "Butterfly",
+  com: "Fine Fragrance Mist",
+  price: "$75",
+  view : "Mix & Match: Buy 3, Get 3 Free or Buy 2, Get 1 Free",
+},
+{
+  image_url : "https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dwf97a6473/crop/026414639_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+  brand : "Fragrance",
+  name : "Butterfly",
+  com: "Fine Fragrance Mist",
+  price: "$75",
+  view : "Mix & Match: Buy 3, Get 3 Free or Buy 2, Get 1 Free",
+},
+{
+  image_url : "https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dwf97a6473/crop/026414639_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+  brand : "Fragrance",
+  name : "Butterfly",
+  com: "Fine Fragrance Mist",
+  price: "$75",
+  view : "Mix & Match: Buy 3, Get 3 Free or Buy 2, Get 1 Free",
+},
+{
+  image_url : "https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dwf97a6473/crop/026414639_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+  brand : "Fragrance",
+  name : "Butterfly",
+  com: "Fine Fragrance Mist",
+  price: "$75",
+  view : "Mix & Match: Buy 3, Get 3 Free or Buy 2, Get 1 Free",
+},
+{
+  image_url : "https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dwf97a6473/crop/026414639_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+  brand : "Fragrance",
+  name : "Butterfly",
+  com: "Fine Fragrance Mist",
+  price: "$75",
+  view : "Mix & Match: Buy 3, Get 3 Free or Buy 2, Get 1 Free",
+},
+{
+  image_url : "https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dwf97a6473/crop/026414639_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+  brand : "Fragrance",
+  name : "Butterfly",
+  com: "Fine Fragrance Mist",
+  price: "$75",
+  view : "Mix & Match: Buy 3, Get 3 Free or Buy 2, Get 1 Free",
+},
+{
+  image_url : "https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dwf97a6473/crop/026414639_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+  brand : "Fragrance",
+  name : "Butterfly",
+  com: "Fine Fragrance Mist",
+  price: "$75",
+  view : "Mix & Match: Buy 3, Get 3 Free or Buy 2, Get 1 Free",
+},
+{
+  image_url : "https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dwf97a6473/crop/026414639_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+  brand : "Fragrance",
+  name : "Butterfly",
+  com: "Fine Fragrance Mist",
+  price: "$75",
+  view : "Mix & Match: Buy 3, Get 3 Free or Buy 2, Get 1 Free",
+},
+{
+  image_url : "https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dwf97a6473/crop/026414639_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+  brand : "Fragrance",
+  name : "Butterfly",
+  com: "Fine Fragrance Mist",
+  price: "$75",
+  view : "Mix & Match: Buy 3, Get 3 Free or Buy 2, Get 1 Free",
+},
+{
+  image_url : "https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dwf97a6473/crop/026414639_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+  brand : "Fragrance",
+  name : "Butterfly",
+  com: "Fine Fragrance Mist",
+  price: "$75",
+  view : "Mix & Match: Buy 3, Get 3 Free or Buy 2, Get 1 Free",
+},
+
+
+
+];
+
+var container = JSON.parse(localStorage.getItem("container")) || [];
+displayData(data);
+
+function displayData(data){
+  data.forEach(function(elem,index){
+    var main_box = document.createElement("div");
+    main_box.setAttribute("id","head")
+
+
+    var img_div = document.createElement("div");
+    img_div.setAttribute("id","main-box")
+    var img = document.createElement("img");
+   
+
+
+    var div1 = document.createElement("div");
+    div1.setAttribute("id","div1");
+
+    var brd = document.createElement("p");
+    brd.setAttribute("class","start")
+    var name = document.createElement("p");
+    name.setAttribute("class","name")
+    var company = document.createElement("p");
+    company.setAttribute("class","company")
+
+
+    var div2 = document.createElement("div");
+    div2.setAttribute("id","div2");
+    var price = document.createElement("p");
+    price.setAttribute("class","price");
+
+
+    var view = document.createElement("p");
+    view.setAttribute("class","view");
+
+var btn = document.createElement("button");
+btn.setAttribute("id","button")
+btn.innerText = "ADD TO BAG";
+btn.addEventListener("click",function(){
+  addToCart(ele)
+})
+
+
+
+
+    img.setAttribute("src",elem.image_url);
+    img_div.append(img);
+
+    brd.innerText = elem.brand;
+    name.innerText=elem.name;
+    company.innerText = elem.com;
+    div1.append(brd,name,company);
+
+    price.innerText = elem.price;
+    view.innerText = elem.view;
+    div2.append(price,view);
+
+    main_box.append(img_div,div1,div2,btn);
+
+document.querySelector("section").append(main_box);
+
+
+
+  })
+
+
+ 
+function addToCart(ele){
+  container.push(ele);
+  localStorage.setItem("container",JSON.stringify(container))
+
+}
+}
